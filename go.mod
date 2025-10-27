01a85b48ce42f4d6f3f8f5f10921bce29fe34411
Lev <levrevera@yandex.ru>
2025-10-27 08:28:41 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/odlev/animal-delivery/contracts
+
+go 1.25.0
+
+require (
+	google.golang.org/grpc v1.76.0
+	google.golang.org/protobuf v1.36.10
+)
+
+require (
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251020155222-88f65dc88635 // indirect
+)
---
+++ status/status.go
@@ -0,0 +1,49 @@
+// Package status provides shared order status constants and helpers.
+package status
+
+import (
+	"fmt"
+
+	pb "github.com/odlev/animal-delivery/contracts/gen/go/animaldelivery"
+)
+
+type OrderStatus string
+
+const (
+	StatusCreated    OrderStatus = "created"
+	StatusInProgress OrderStatus = "in_progress"
+	StatusCompleted  OrderStatus = "completed"
+	StatusDeleted    OrderStatus = "deleted"
+)
+
+var statusToProto = map[OrderStatus]pb.OrderStatus{
+	StatusCreated:    pb.OrderStatus_ORDER_STATUS_CREATED,
+	StatusInProgress: pb.OrderStatus_ORDER_STATUS_IN_PROGRESS,
+	StatusCompleted:  pb.OrderStatus_ORDER_STATUS_COMPLETED,
+	StatusDeleted:    pb.OrderStatus_ORDER_STATUS_DELETED,
+}
+
+var protoToStatus = map[pb.OrderStatus]OrderStatus{
+	pb.OrderStatus_ORDER_STATUS_CREATED:     StatusCreated,
+	pb.OrderStatus_ORDER_STATUS_IN_PROGRESS: StatusInProgress,
+	pb.OrderStatus_ORDER_STATUS_COMPLETED:   StatusCompleted,
+	pb.OrderStatus_ORDER_STATUS_DELETED:     StatusDeleted,
+}
+
+func (s OrderStatus) String() string {
+	return string(s)
+}
+
+func (s OrderStatus) ToPb() (pb.OrderStatus, error) {
+	if v, ok := statusToProto[s]; ok {
+		return v, nil
+	}
+	return pb.OrderStatus_ORDER_STATUS_UNSPECIFIED, fmt.Errorf("unknown order status %q", s)
+}
+
+func FromPb(p pb.OrderStatus) OrderStatus {
+	if v, ok := protoToStatus[p]; ok {
+		return v
+	}
+	return StatusCreated
+}
